be41d6c0b581f9561977607161bac554704ac437
Arbahud Rio Daroyni <roydotendless@gmail.com>
2021-05-11 09:04:14 +0700

    update

--- template-parts/components/navbar.php
+++ template-parts/components/navbar.php
@@ -5,113 +5,47 @@
       <img src="<?= get_logo() ?>" alt="JejakCyber" width="199">
       </a>
       <ul class="nk-nav nk-nav-right d-none d-lg-table-cell" data-nav-mobile="#nk-nav-mobile">
-        <li class="nk-drop-item">
-          <a href="elements.html">Features</a>
+        <li class=" nk-drop-item">
+          <a href="tournaments.html">Features</a>
           <ul class="dropdown">
-            <li><a href="elements.html">Elements (Shortcodes)</a></li>
+            <li><a href="tournaments.html">Tournament</a></li>
+            <li><a href="tournaments-teams.html">Teams</a></li>
           </ul>
         </li>
         <li class="active nk-drop-item">
-          <a href="blog-list.html">
-          Blog
-          </a>
+          <a href="blog-list.html">Blog</a>
           <ul class="dropdown">
-            <li>
-              <a href="news.html">
-              News
-              </a>
-            </li>
-            <li class=" nk-drop-item">
-              <a href="blog-grid.html">
-              Blog With Sidebar
-              </a>
+            <li><a href="news.html">News</a></li>
+            <li class="active"><a href="blog-article.html">Review</a></li>
+            <li class="active"><a href="blog-article.html">Preview</a></li>
+            <li class="active"><a href="blog-article.html">Guides</a></li>
+            <!-- <li class=" nk-drop-item">
+              <a href="blog-grid.html">Blog With Sidebar</a>
               <ul class="dropdown">
-                <li>
-                  <a href="blog-grid.html">
-                  Blog Grid
-                  </a>
-                </li>
-                <li>
-                  <a href="blog-list.html">
-                  Blog List
-                  </a>
-                </li>
+                <li><a href="blog-grid.html">Blog Grid</a></li>
+                <li><a href="blog-list.html">Blog List</a></li>
               </ul>
             </li>
-            <li>
-              <a href="blog-fullwidth.html">
-              Blog Fullwidth
-              </a>
-            </li>
-            <li class="active">
-              <a href="blog-article.html">
-              Review
-              </a>
-            </li>
+            <li><a href="blog-fullwidth.html">Blog Fullwidth</a></li> -->
           </ul>
         </li>
-        <li>
-          <a href="gallery.html">
-          Gallery
-          </a>
-        </li>
-        <li class=" nk-drop-item">
-          <a href="tournaments.html">
-          Tournaments
-          </a>
+        <li class="nk-drop-item">
+          <a href="elements.html">Gallery</a>
           <ul class="dropdown">
-            <li>
-              <a href="tournaments.html">
-              Tournament
-              </a>
-            </li>
-            <li>
-              <a href="tournaments-teams.html">
-              Teams
-              </a>
-            </li>
-            <li>
-              <a href="tournaments-teammate.html">
-              Teammate
-              </a>
-            </li>
+            <li><a href="elements.html">Video</a></li>
+            <li><a href="elements.html">Photo</a></li>
           </ul>
         </li>
+        <li><a href="gallery.html">Download</a></li>
         <li class=" nk-drop-item">
-          <a href="store.html">
-          Store
-          </a>
+          <a href="store.html">Store</a>
           <ul class="dropdown">
-            <li>
-              <a href="store.html">
-              Store
-              </a>
-            </li>
-            <li>
-              <a href="store-product.html">
-              Product
-              </a>
-            </li>
-            <li>
-              <a href="store-catalog.html">
-              Catalog
-              </a>
-            </li>
-            <li>
-              <a href="store-catalog-alt.html">
-              Catalog Alt
-              </a>
-            </li>
-            <li>
-              <a href="store-checkout.html">
-              Checkout
-              </a>
-            </li>
-            <li>
-              <a href="store-cart.html">
-              Cart
-              </a>
-            </li>
+            <li><a href="store.html">Store</a></li>
+            <li><a href="store-product.html">Product</a></li>
+            <li><a href="store-catalog.html">Catalog</a></li>
+            <li><a href="store-catalog-alt.html">Catalog Alt</a></li>
+            <li><a href="store-checkout.html">Checkout</a></li>
+            <li><a href="store-cart.html">Cart</a></li>
           </ul>
         </li>
       </ul>
